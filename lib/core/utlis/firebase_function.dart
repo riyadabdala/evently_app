b4186d35144b19443ac317cb2b807d448e572fa9
tch <riyadelkholy364@gmail.com>
2025-01-27 19:58:32 +0200

    create project files splash view                        login view create Account view forget password view Authentication

--- lib/core/utlis/firebase_function.dart
+++ lib/core/utlis/firebase_function.dart
@@ -1,11 +1,23 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 
-class FirebaseFunctions{
-  static createAccount(String email,String password,){
-     FirebaseAuth.instance.createUserWithEmailAndPassword(
-         email: email,
-         password: password);
+class FirebaseFunctions {
+  static createAccount(String emailAddress,
+      String password,) async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        // print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        // print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 }
-
